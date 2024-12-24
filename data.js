d292b52a4cc59314c6c48dce72280522ed54f4e0
rcr1974 <rcr74.formacion@gmail.com>
2024-12-24 12:54:57 +0100

    Add files via upload

---
+++ data.js
@@ -0,0 +1,400 @@
+const data=
+{
+  "temas": [
+    {
+      "nombre": "IA",
+      "subtemas": [
+        {
+          "nombre": "Genéricos",
+          "enlaces": [
+            {
+              "titulo": "Mistral",
+              "url": "https://mistral.ai/",
+              "descripcion": "LLM Europeo gratuito",
+              "level": "high"
+            },
+            {
+              "titulo": "DeepSeek",
+              "url": "https://chat.deepseek.com/",
+              "descripcion": "Mejor que el modelo 0 de Chatgpt",
+              "level": "high"
+            },
+            {
+              "titulo": "ClaudeAI",
+              "url": "https://claude.ai/",
+              "descripcion": "",
+              "level": "high"
+            },
+            {
+              "titulo": "Gemini",
+              "url": "https://gemini.google.com/app",
+              "descripcion": "De Google",
+              "level": "normal"
+            },
+            {
+              "titulo": "Copilot",
+              "url": "https://copilot.microsoft.com/",
+              "descripcion": "Microsoft",
+              "level": "normal"
+            },
+            {
+              "titulo": "Grok",
+              "url": "https://x.com/i/grok",
+              "descripcion": "Permite hacer fotos, video, código,...",
+              "level": "normal"
+            },
+            {
+              "titulo": "ChatGPT",
+              "url": "https://chatgpt.com/",
+              "descripcion": "",
+              "level": "high"
+            },
+            {
+              "titulo": "Phind",
+              "url": "https://www.phind.com/",
+              "descripcion": "",
+              "level": "normal"
+            }
+          ]
+        },
+        {
+          "nombre": "Programación",
+          "enlaces": [
+            {
+              "titulo": "ClaudeAI",
+              "url": "https://claude.ai/",
+              "descripcion": "",
+              "level": "high"
+            },
+            {
+              "titulo": "Github copilot",
+              "url": "https://github.com/copilot",
+              "descripcion": "",
+              "level": "high"
+            },
+            {
+              "titulo": "v0",
+              "url": "https://v0.dev/",
+              "descripcion": "",
+              "level": "normal"
+            },
+            {
+              "titulo": "Bolt",
+              "url": "https://bolt.new/",
+              "descripcion": "Permite crear proyectos completos",
+              "level": "normal"
+            },
+            {
+              "titulo": "Google studio",
+              "url": "https://aistudio.google.com/live",
+              "descripcion": "",
+              "level": "high"
+            },
+            {
+              "titulo": "Websim",
+              "url": "https://websim.ai/",
+              "descripcion": "",
+              "level": "normal"
+            }
+          ]
+        },
+        {
+          "nombre": "Legal",
+          "enlaces": [
+            {
+              "titulo": "Justicio",
+              "url": "https://justicio.es/",
+              "descripcion": "",
+              "level": "normal"
+            }
+          ]
+        },
+        {
+          "nombre": "Imágenes & Videos",
+          "enlaces": [
+            {
+              "titulo": "Fliki",
+              "url": "https://fliki.ai/",
+              "descripcion": "",
+              "level": "normal"
+            },
+            {
+              "titulo": "Piclumen",
+              "url": "https://piclumen.com/app/",
+              "descripcion": "",
+              "level": "normal"
+            },
+            {
+              "titulo": "Lexica",
+              "url": "https://lexica.art",
+              "descripcion": "",
+              "level": "normal"
+            },
+            {
+              "titulo": "Redpandaai",
+              "url": "https://redpandaai.com",
+              "descripcion": "",
+              "level": "normal"
+            },
+            {
+              "titulo": "Blinkshot",
+              "url": "https://www.blinkshot.io/",
+              "descripcion": "",
+              "level": "normal"
+            },
+            {
+              "titulo": "Kritaaidiffusion",
+              "url": "https://kritaaidiffusion.com/es",
+              "descripcion": "",
+              "level": "normal"
+            },
+            {
+              "titulo": "Freepik",
+              "url": "https://www.freepik.com/pikaso/ai-image-generator",
+              "descripcion": "",
+              "level": "normal"
+            }
+          ]
+        },
+        {
+          "nombre": "Prompts",
+          "enlaces": [
+            {
+              "titulo": "Foroprompt",
+              "url": "https://foroprompt.com/",
+              "descripcion": "",
+              "level": "normal"
+            },
+            {
+              "titulo": "Prompthero",
+              "url": "https://prompthero.com/",
+              "descripcion": "",
+              "level": "normal"
+            },
+            {
+              "titulo": "Promptden",
+              "url": "https://promptden.com/",
+              "descripcion": "",
+              "level": "normal"
+            },
+            {
+              "titulo": "Workmind",
+              "url": "https://workmind.ai/ai-prompt-library/",
+              "descripcion": "",
+              "level": "normal"
+            },
+            {
+              "titulo": "Prompt Library",
+              "url": "https://promptlibrary.org/",
+              "descripcion": "",
+              "level": "normal"
+            },
+            {
+              "titulo": "Aimind",
+              "url": "https://www.aimind.so/prompt-generator?email=",
+              "descripcion": "",
+              "level": "normal"
+            },
+            {
+              "titulo": "Prompthero",
+              "url": "https://prompthero.com/",
+              "descripcion": "",
+              "level": "normal"
+            }
+          ]
+        },
+        {
+          "nombre": "Otros",
+          "enlaces": [
+            {
+              "titulo": "IAPerfecta",
+              "url": "https://iaperfecta.com/ ",
+              "descripcion": "Buscador de IAs",
+              "level": "normal"
+            },
+            {
+              "titulo": "Giz",
+              "url": "https://www.giz.ai/",
+              "descripcion": "Puedes ejecutar el modelo que selecciones",
+              "level": "normal"
+            },
+            {
+              "titulo": "Perplexity",
+              "url": "https://www.perplexity.ai/",
+              "descripcion": "Buscador con IA",
+              "level": "normal"
+            }
+          ]
+        }
+      ]
+    },
+    {
+      "nombre": "Programación",
+      "subtemas": [
+        {
+          "nombre": "Python",
+          "enlaces": [
+            {
+              "titulo": "Python.org",
+              "url": "https://www.python.org/",
+              "descripcion": "Sitio oficial de Python",
+              "level": "normal"
+            },
+            {
+              "titulo": "Real Python",
+              "url": "https://realpython.com/",
+              "descripcion": "Tutoriales y artículos sobre Python",
+              "level": "normal"
+            }
+          ]
+        },
+        {
+          "nombre": "JavaScript",
+          "enlaces": [
+            {
+              "titulo": "MDN JavaScript",
+              "url": "https://developer.mozilla.org/es/docs/Web/JavaScript",
+              "descripcion": "Guía completa de JavaScript",
+              "level": "normal"
+            },
+            {
+              "titulo": "JavaScript.info",
+              "url": "https://javascript.info/",
+              "descripcion": "Tutorial moderno de JavaScript",
+              "level": "normal"
+            }
+          ]
+        }
+      ]
+    },
+    {
+      "nombre": "Formación",
+      "subtemas": [
+        {
+          "nombre": "Webs formación",
+          "enlaces": [
+            {
+              "titulo": "Udemy",
+              "url": "https://www.udemy.com/",
+              "descripcion": "",
+              "level": "normal"
+            },
+            {
+              "titulo": "IBM SkillsBuild",
+              "url": "https://sb-auth.skillsbuild.org/login?client_id=yl-internal-adopter&learning_path=https:%2F%2Fskills.yourlearning.ibm.com%2Factivity%2FPLAN-01FB8819682C&redirect_uri=https:%2F%2Fskills.yourlearning.ibm.com%2Foauth%2Fcallback&referer=skills.yourlearning.ibm.com&response_type=code&scope=openid%20email",
+              "descripcion": "",
+              "level": "normal"
+            }
+          ]
+        },
+        {
+          "nombre": "Cursos",
+          "enlaces": [
+            {
+              "titulo": "Ciencia de Datos y Analítica de Negocio con Python y ChatGPT",
+              "url": "https://skills.yourlearning.ibm.com/activity/UDEMY-5320790",
+              "descripcion": "Udemy curso de SkillsBuild",
+              "level": "high"
+            },
+            {
+              "titulo": "Zero to Mastery TensorFlow for Deep Learning",
+              "url": "https://dev.mrdbourke.com/tensorflow-deep-learning/",
+              "descripcion": "Repositorio Github con todo el curso, pdf, slides, codigo, ...",
+              "level": "normal"
+            },
+            {
+              "titulo": "DataTalks Club",
+              "url": "https://datatalks.club/blog/guide-to-free-online-courses-at-datatalks-club.html",
+              "descripcion": "Formacion deep learning, llms, ...",
+              "level": "normal"
+            }
+          ]
+        }        
+      ]
+    },
+    {
+      "nombre": "Youtube",
+      "subtemas": [
+        {
+          "nombre": "IA",
+          "enlaces": [
+            {
+              "titulo": "Physics.org",
+              "url": "https://www.physics.org/",
+              "descripcion": "Portal de recursos sobre física",
+              "level": "normal"
+            },
+            {
+              "titulo": "Hyperphysics",
+              "url": "http://hyperphysics.phy-astr.gsu.edu/hbase/index.html",
+              "descripcion": "Conceptos de física explicados",
+              "level": "normal"
+            }
+          ]
+        },
+        {
+          "nombre": "Inglés",
+          "enlaces": [
+            {
+              "titulo": "Chemistry LibreTexts",
+              "url": "https://chem.libretexts.org/",
+              "descripcion": "Biblioteca de recursos de química",
+              "level": "normal"
+            },
+            {
+              "titulo": "Royal Society of Chemistry",
+              "url": "https://www.rsc.org/",
+              "descripcion": "Recursos educativos de química",
+              "level": "normal"
+            }
+          ]
+        },
+        {
+          "nombre": "Música",
+          "enlaces": [
+            {
+              "titulo": "Chemistry LibreTexts",
+              "url": "https://chem.libretexts.org/",
+              "descripcion": "Biblioteca de recursos de química",
+              "level": "normal"
+            },
+            {
+              "titulo": "Royal Society of Chemistry",
+              "url": "https://www.rsc.org/",
+              "descripcion": "Recursos educativos de química",
+              "level": "normal"
+            }
+          ]
+        }
+      ]
+    },
+    {
+      "nombre": "Plantilla",
+      "subtemas": [
+        {
+          "nombre": "Seccion1",
+          "enlaces": [
+            {
+              "titulo": "Titulo1",
+              "url": "https://www.google.es/",
+              "descripcion": "Bla, bla",
+              "level": "normal"
+            }
+          ]
+        },
+        {
+          "nombre": "Seccion2",
+          "enlaces": [
+            {
+              "titulo": "Titulo1",
+              "url": "https://www.google.es/",
+              "descripcion": "Bla, bla",
+              "level": "normal"
+            }
+          ]
+        }        
+      ]
+    }
+  ]
+}
+;
